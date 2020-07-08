ba847e8413232eba57939f60eba3c2fcd56d0b41
Zeki Kadiroğlu <z.kdrglu07@gmail.com>
2020-07-08 20:53:17 +0300

    düzeltme

--- assignment2-covid19.py
+++ assignment2-covid19.py
@@ -1,5 +1,8 @@
-name = input("name : ")
-if name == "Joseph":
-    print("Hello, Joseph! The password is : W@12,")
-else:
-    print("Hello,", name + "!", "See you later.")
+age = int(input("age : "))
+cigarette = True
+chronic = input("chronic : ")
+immune = input("is your immune system too weak? : ")
+if age > 75 and bool(cigarette) and chronic == "yes" and immune == "yes":
+    print("You are in risky grup")
+else :
+    print("You are not  in risky grup")
