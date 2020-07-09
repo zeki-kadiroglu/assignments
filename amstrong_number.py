50ae1e4a18bd91189286b759f8026154f4ce8f02
Zeki Kadiroğlu <z.kdrglu07@gmail.com>
2020-07-09 12:43:08 +0300

    amstrong assignment

--- amstrong_number.py
+++ amstrong_number.py
@@ -1,4 +1,12 @@
 number = input("enter positive integer : ")
-
-
-
+list = list(number)
+amstrong = 0
+total = []
+for i in number :
+    amstrong = int(i) ** len(list)
+    i += i
+    total.append(amstrong)
+if sum(total) == int(number):
+    print(number," : is amstrong number")
+else:
+    print(number, ": is not amstrong number")
